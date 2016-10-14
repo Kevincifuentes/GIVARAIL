acfcd211d683ba64108f9c11f700734a48151581
KevinCifuentes <kevin.cifuentes@opendeusto.es>
2016-10-14 12:58:20 +0200

    Programming code related to barometer modification

--- filtradoGPSIMU.py
+++ filtradoGPSIMU.py
@@ -2,16 +2,26 @@
 
 import math
 import atexit
-import time
+import datetime
 import redis
 import json
 import numpy as np
 
+def procesoExtra():
+    variable = 10
+    i = 0
+    while i < 10000:
+        variable = variable * 16
+        i = i+1
+
 
 almacenamientoRedis = redis.StrictRedis(host='localhost', port=6379, db=0)
-
+tiempoActual = datetime.now().strftime("%d%m%y_%H%M%S%f")
+nombreFichero = '/media/card/valoresPrueba_'+ tiempoActual +'.txt'
+fichero = open(nombreFichero, "wb")
+contador = 0;
 while(True):
-
+    contador = contador + 1
     valoresIMU = almacenamientoRedis.rpop('cola_imu')
     posicionIMU = None
 
@@ -22,6 +32,24 @@ while(True):
         if(valoresIMU != None):
             posicionIMU = json.loads(valoresIMU)
             #>>>>>>>>>>>>>>>>>KALMAN con IMU
+            procesoExtra()
+            #gnssN, gnssE, gnssU, accX, accY, accZ, gyrX, gyrY, gyrZ, magX, magY, magZ, bar, Fecha;
+            aceleracionX = posicionIMU["Acceleration"]["accX"]
+            aceleracionY = posicionIMU["Acceleration"]["accY"]
+            aceleracionZ = posicionIMU["Acceleration"]["accZ"]
+            giroscopioX = posicionIMU["Angular Velocity"]["gyrX"]
+            giroscopioY = posicionIMU["Angular Velocity"]["gyrY"]
+            giroscopioZ = posicionIMU["Angular Velocity"]["gyrZ"]
+            magnetometroX = posicionIMU["Magnetic"]["magX"]
+            magnetometroY = posicionIMU["Magnetic"]["magY"]
+            magnetometroZ = posicionIMU["Magnetic"]["magZ"]
+            barometro = posicionIMU["Pressure"]
+
+            if contador == 100:
+                contador = 0
+                fichero.write("0,0,0,"+aceleracionX+","+aceleracionY+","+aceleracionZ+","+giroscopioX+","+giroscopioY+","+giroscopioZ+","+magnetometroX+","+magnetometroY+","+magnetometroZ+","+barometro+",0;\n")
+            else:
+                fichero.write("0,0,0,"+aceleracionX+","+aceleracionY+","+aceleracionZ+","+giroscopioX+","+giroscopioY+","+giroscopioZ+","+magnetometroX+","+magnetometroY+","+magnetometroZ+","+barometro+","+";\n")
             print("Solo IMU:")
             #print("FILTRADO:"+str(posicionIMU))
         else:
@@ -32,7 +60,19 @@ while(True):
             print("ERROR: No hay valores de la IMU. Cerrando...")
             exit(0)
         posicionIMU = json.loads(valoresIMU)
+
+        aceleracionX = posicionIMU["Acceleration"]["accX"]
+        aceleracionY = posicionIMU["Acceleration"]["accY"]
+        aceleracionZ = posicionIMU["Acceleration"]["accZ"]
+        giroscopioX = posicionIMU["Angular Velocity"]["gyrX"]
+        giroscopioY = posicionIMU["Angular Velocity"]["gyrY"]
+        giroscopioZ = posicionIMU["Angular Velocity"]["gyrZ"]
+        magnetometroX = posicionIMU["Magnetic"]["magX"]
+        magnetometroY = posicionIMU["Magnetic"]["magY"]
+        magnetometroZ = posicionIMU["Magnetic"]["magZ"]
+        barometro = posicionIMU["Pressure"]
         print("Los dos:")
+
         #>>>>>>>>>>>>>>>>>KALMAN con IMU y GPS
         #print("FILTRADO:"+str(posicionGPS))
         #print("FILTRADO:"+str(posicionIMU))
